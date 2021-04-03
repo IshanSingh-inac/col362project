c0c41d6d4b0963b6c7116f432df03659cfa5456c
Siddharth Grover <cs1180392@iitd.ac.in>
2021-04-03 16:44:41 +0530

    cascade on delete

--- database/build.sql
+++ database/build.sql
@@ -25,13 +25,21 @@ CREATE TABLE favourites(
     id INTEGER,
     ticker VARCHAR,
     constraint fav_key primary key (id,ticker),
-    constraint id_ref foreign key (id) references users(id),
+    constraint id_ref foreign key (id) references users(id) on delete cascade,
     constraint ticker_ref foreign key (ticker) references tickers(ticker)
+);
+
+CREATE TABLE following(
+    id1 INTEGER,
+    id2 INTEGER,
+    constraint fol_key primary key (id1,id2),
+    constraint id1_ref foreign key (id1) references users(id) on delete cascade,
+    constraint id2_ref foreign key (id2) references users(id) on delete cascade
 );
 
 \copy users from './users.csv' delimiter ',' csv header;
 \copy tickers from './tickers.csv' delimiter ',' csv header;
 \copy stocks from './stocks.csv' delimiter ',' csv header;
 \copy favourites from './favourites.csv' delimiter ',' csv header;
-
+\copy following from './following.csv' delimiter ',' csv header;
  
